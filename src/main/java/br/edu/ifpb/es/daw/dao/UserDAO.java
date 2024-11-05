fb7ff499ab6cd3335bea545afd5952c4382fec5e
Jaindson Valentim Santana <jaindson.santana.ext@synchro.com.br>
2024-11-05 10:11:30 -0300

    Implementação do UserDAO usando o try-with-resources no EntityManager.

--- src/main/java/br/edu/ifpb/es/daw/dao/UserDAO.java
+++ src/main/java/br/edu/ifpb/es/daw/dao/UserDAO.java
@@ -12,89 +12,83 @@ import br.edu.ifpb.es.daw.entities.User;
 public class UserDAO extends DAO {
 
 	public void save(User user) throws PersistenciaDawException {
-		EntityManager em = getEntityManager();
-		EntityTransaction transaction = em.getTransaction();
-		transaction.begin();
-		try {
-			em.persist(user);
-			transaction.commit();
-		} catch (PersistenceException pe) {
-			pe.printStackTrace();
-			if (transaction.isActive()) {
-				transaction.rollback();
+		try(EntityManager em = getEntityManager()) {
+			EntityTransaction transaction = em.getTransaction();
+			transaction.begin();
+			try {
+				em.persist(user);
+				transaction.commit();
+			} catch (PersistenceException pe) {
+				pe.printStackTrace();
+				if (transaction.isActive()) {
+					transaction.rollback();
+				}
+				throw new PersistenciaDawException("Ocorreu algum erro ao tentar salvar o usuário.", pe);
 			}
-			throw new PersistenciaDawException("Ocorreu algum erro ao tentar salvar o usuário.", pe);
-		} finally {
-			em.close();
 		}
 	}
 
 	public User update(User user) throws PersistenciaDawException {
-		EntityManager em = getEntityManager();
-		EntityTransaction transaction = em.getTransaction();
-		transaction.begin();
-		User resultado = user;
-		try {
-			resultado = em.merge(user);
-			transaction.commit();
-		} catch (PersistenceException pe) {
-			pe.printStackTrace();
-			if (transaction.isActive()) {
-				transaction.rollback();
+		try(EntityManager em = getEntityManager()) {
+			EntityTransaction transaction = em.getTransaction();
+			transaction.begin();
+			User resultado = user;
+			try {
+				resultado = em.merge(user);
+				transaction.commit();
+			} catch (PersistenceException pe) {
+				pe.printStackTrace();
+				if (transaction.isActive()) {
+					transaction.rollback();
+				}
+				throw new PersistenciaDawException("Ocorreu algum erro ao tentar atualizar o usuário.", pe);
 			}
-			throw new PersistenciaDawException("Ocorreu algum erro ao tentar atualizar o usuário.", pe);
-		} finally {
-			em.close();
+			return resultado;
 		}
-		return resultado;
 	}
 
 	public void delete(User user) throws PersistenciaDawException {
-		EntityManager em = getEntityManager();
-		EntityTransaction transaction = em.getTransaction();
-		transaction.begin();
-		try {
-			user = em.find(User.class, user.getId());
-			em.remove(user);
-			transaction.commit();
-		} catch (PersistenceException pe) {
-			pe.printStackTrace();
-			if (transaction.isActive()) {
-				transaction.rollback();
+		try(EntityManager em = getEntityManager()) {
+			EntityTransaction transaction = em.getTransaction();
+			transaction.begin();
+			try {
+				user = em.find(User.class, user.getId());
+				em.remove(user);
+				transaction.commit();
+			} catch (PersistenceException pe) {
+				pe.printStackTrace();
+				if (transaction.isActive()) {
+					transaction.rollback();
+				}
+				throw new PersistenciaDawException("Ocorreu algum erro ao tentar remover o usuário.", pe);
 			}
-			throw new PersistenciaDawException("Ocorreu algum erro ao tentar remover o usuário.", pe);
-		} finally {
-			em.close();
 		}
 	}
 
 	public User getByID(int userId) throws PersistenciaDawException {
-		EntityManager em = getEntityManager();
-		User resultado = null;
-		try {
-			resultado = em.find(User.class, userId);
-		} catch (PersistenceException pe) {
-			pe.printStackTrace();
-			throw new PersistenciaDawException("Ocorreu algum erro ao tentar recuperar o usuário com base no ID.", pe);
-		} finally {
-			em.close();
+		try(EntityManager em = getEntityManager()) {
+			User resultado = null;
+			try {
+				resultado = em.find(User.class, userId);
+			} catch (PersistenceException pe) {
+				pe.printStackTrace();
+				throw new PersistenciaDawException("Ocorreu algum erro ao tentar recuperar o usuário com base no ID.", pe);
+			}
+			return resultado;
 		}
-
-		return resultado;
 	}
 
 	public List<User> getAll() throws PersistenciaDawException {
-		EntityManager em = getEntityManager();
-		List<User> resultado = null;
-		try {
-			TypedQuery<User> query = em.createQuery("SELECT u FROM User u", User.class);
-			resultado = query.getResultList();
-		} catch (PersistenceException pe) {
-			pe.printStackTrace();
-			throw new PersistenciaDawException("Ocorreu algum erro ao tentar recuperar todos os usuários.", pe);
-		} finally {
-			em.close();
+		try(EntityManager em = getEntityManager()) {
+			List<User> resultado = null;
+			try {
+				TypedQuery<User> query = em.createQuery("SELECT u FROM User u", User.class);
+				resultado = query.getResultList();
+			} catch (PersistenceException pe) {
+				pe.printStackTrace();
+				throw new PersistenciaDawException("Ocorreu algum erro ao tentar recuperar todos os usuários.", pe);
+			}
+			return resultado;
 		}
-		return resultado;
 	}
 }
